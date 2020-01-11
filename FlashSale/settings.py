8411645c0bd6c6a60d0ea27d84a4033d6aa18159
nvvu99 <34371770+nvvu99@users.noreply.github.com>
2020-01-11 08:36:00 +0700

    Update

--- FlashSale/settings.py
+++ FlashSale/settings.py
@@ -19,18 +19,18 @@ TIKI_FLASHSALE = 'https://tiki.vn/deal-hot?tab=now&page={}'
 TIKI_API = [
     'https://tiki.vn/api/v2/events/deals/?category_ids={}&type=now&page={}&per_page=20',
     'https://tiki.vn/api/v2/events/deals/?category_ids={}&type=coming&page={}&per_page=20',
-    ]
+]
 SHOPEE = 'https://shoppe.vn'
 SHOPEE_API = [
     'https://shopee.vn/api/v2/flash_sale/get_all_sessions'
     'https://shopee.vn/api/v2/flash_sale/flash_sale_get_items?',
-    ]
+]
 LAZADA = 'https://lazada.vn'
 SENDO_API = [
     'https://api.sendo.vn/flash-deal/category-group?special_status=0',
     'https://api.sendo.vn/flash-deal/ajax-deal/',
     'https://api.sendo.vn/flash-deal/ajax-product/',
-    ]
+]
 CHROME_PATH = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe'
 MONGO_SERVER = '127.0.0.1'
 MONGO_PORT = 27017
@@ -44,72 +44,72 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'FlashSale.middlewares.FlashsaleSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 # DOWNLOADER_MIDDLEWARES = {
-   # 'FlashSale.middlewares.FlashsaleSeleniumDownloaderMiddleware': 543,
-   # 'FlashSale.middlewares.FlashsaleRequestsDownloaderMiddleware': 543,
+# 'FlashSale.middlewares.FlashsaleSeleniumDownloaderMiddleware': 543,
+# 'FlashSale.middlewares.FlashsaleRequestsDownloaderMiddleware': 543,
 # }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'FlashSale.pipelines.DuplicateItemPipeline': 200,
     'FlashSale.pipelines.ToDatabasePipeline': 300,
-    # 'FlashSale.pipelines.ToJsonFilePipeline': 400,
+    'FlashSale.pipelines.ToJsonFilePipeline': 400,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
